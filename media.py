9e5ccfd481cb41876831500987256cea78ac0471
jasmine81 <38848939+jasmine81@users.noreply.github.com>
2018-05-25 23:39:00 +0530

    Update media.py

--- media.py
+++ media.py
@@ -4,14 +4,14 @@ import webbrowser
 
 class Car():
     VALID_RATINGS = ["EXCELLENT", "GOOD", "BAD", "AVERAGE"]
+    # This class created a new car object and sets its attributes
 
-
-def __init__(self, car_title, car_storyline, poster_image, trailer_youtube):
+    def __init__"
+    "(self, car_title, car_storyline, poster_image, trailer_youtube):
         self.title = car_title
         self.storyline = car_storyline
         self.poster_img_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-
-def show_trailer(self):
+    def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
