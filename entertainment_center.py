4f078eb15aca2361e47afe28e314e8275f82c147
jasmine81 <38848939+jasmine81@users.noreply.github.com>
2018-07-04 10:56:17 +0530

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,36 +2,36 @@
 import media  # importing packages
 import fresh_tomatoes  # importing packages
 # creating variables
-hondacity = media.Car("Honda City", "luxury",
+hondacity = media.Car("Honda City",
                       "http://www.loannow.in/wp-content/uploads"
                       "/2015/01/next-gen"
                       "eration-honda-city-2014.jpg",
                       "https://www.youtube.com/embed/8ORwzCqfkTo")
-mercedesbenz = media.Car("Mercedes Benz", "comfort",
+mercedesbenz = media.Car("Mercedes Benz",
                          "https://cars.usnews.com/static/images/Auto/izmo/i33"
                          "960660/2018_mercedes_benz_e_class_angularfront.jpg",
                          "https://www.youtube.com/embed/JbA0u-OpoLw")
-verna = media.Car("Verna", "happiness",
+verna = media.Car("Verna",
                   "https://images.cardekho.com/images/car-images/large"
                   "/Hyundai/Verna/03.jpg",
                   "https://www.youtube.com/embed/HmIBqEW3B8o")
-amaze = media.Car("Amaze", "smilingface car",
+amaze = media.Car("Amaze",
                   "https://img.gaadicdn.com/images/car-images/large/Honda/"
                   "Honda-Amaze/honda-amaze-urban-titanium-metallic.jpg",
                   "https://www.youtube.com/embed/DasUUC2eCD8")
-ciaz = media.Car("Ciaz", "posh",
+ciaz = media.Car("Ciaz",
                  "https://ymimg1.b8cdn.com/uploads/car_model/3994/logo/"
                  "2018_Suzuki_Ciaz.jpg",
                  "https://www.youtube.com/embed/wsGrHou5leU")
-xcent = media.Car("Xcent", "enjoyment",
+xcent = media.Car("Xcent",
                   "https://auto.ndtvimg.com/car-images/gallery/hyundai/xcent/"
                   "exterior/hyundai-xcent-front.jpg",
                   "https://www.youtube.com/embed/8QraweGoIMs")
-zest = media.Car("Zest", "tour",
+zest = media.Car("Zest",
                  "https://auto.ndtvimg.com/car-images/colors/tata/zest/tata"
                  "-zest-dune-beige.jpg?v=",
                  "https://www.youtube.com/embed/sl9zcOwTOpU")
-skoda = media.Car("Skoda", "stylish",
+skoda = media.Car("Skoda",
                   "https://img0.gaadicdn.com/images/car-images/large/Skoda/"
                   "Skoda-Superb/047.jpg",
                   "https://www.youtube.com/embed/Ihsn6oLYegY")
